aa5e555a8c7156c9a832e084f1465629b7d9c108
Billy Ponto <bpgponto@gmail.com>
2022-12-10 08:49:46 +0800

    style: update desktop ui

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -9,26 +9,14 @@ $(document).ready(function () {
 		});
 	});
 
-	// user avatar dropdown
-	// var dd_btn = $(".dropdown-button")
-	// var dd_stream = $("#dropdown-stream")
-	// dd_btn.click(function () {
-	//     dd_btn.each(function () {
-	//         if (dd_stream.hasClass("hidden")) {
-	//             dd_stream.removeClass("hidden").addClass("block");
-	//         } else if (dd_stream.hasClass("block")) {
-	//             dd_stream.removeClass("block").addClass("hidden");
-	//         }
-	//     })
-	// })
-
 	var buttonFaq = document.querySelectorAll(".button-faq");
 	for (let index = 0; index < buttonFaq.length; index++) {
 		buttonFaq[index].addEventListener("click", (e) => {
 			var idButton = e.currentTarget.getAttribute("id");
 			var faqContent = document.querySelector(`#${idButton}-content`);
-			console.log(faqContent);
+			// console.log(faqContent);
 			faqContent.classList.toggle("hidden");
+			document.querySelector(`#${idButton} img`).classList.toggle("rotate-180");
 		});
 	}
 });
