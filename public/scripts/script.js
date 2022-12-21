45cecbee170358019a5dc083632f6fae91e94a51
Billy Ponto <bpgponto@gmail.com>
2022-12-21 17:23:58 +0800

    feat: update new pages
    - success payment
    - sign in
    - sign up

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,9 +1,11 @@
 // Navbar Toggler
-var navbarToggler = document.getElementById("navbarToggler");
-var navbarMenu = document.getElementById("navigation");
-navbarToggler.addEventListener("click", (e) => {
-	e.preventDefault();
-	navbarMenu.classList.toggle("hidden");
+$(".mobileMenuButton").each(function (_, navToggler) {
+	var target = $(navToggler).data("target");
+	$(navToggler).on("click", function () {
+		$(target).animate({
+			height: "toggle",
+		});
+	});
 });
 
 // FAQ
@@ -24,3 +26,29 @@ for (let index = 0; index < buttonFaq.length; index++) {
 function toggleDropdown(el) {
 	document.getElementById("userDropdownMenu").classList.toggle("hidden");
 }
+
+// Upload user photo
+$("#btnUploadPhoto").on("click", () => {
+	$("input#photo").trigger("click");
+});
+
+// Delete photo preview
+$("#btnDeletePhoto").on("click", () => {
+	$("#imageSrc").attr("src", "../assets/svgs/ic-default-photo.svg");
+	$("#btnDeletePhoto").toggleClass("hidden");
+	$("#btnUploadPhoto").toggleClass("hidden");
+});
+
+// Show photo preview
+$("#photo").change(function (e) {
+	if (this.files && this.files[0]) {
+		var reader = new FileReader();
+		reader.onload = function (e) {
+			$("#imageSrc").attr("src", e.target.result);
+		};
+		reader.readAsDataURL(this.files[0]);
+	}
+
+	$("#btnDeletePhoto").toggleClass("hidden");
+	$("#btnUploadPhoto").toggleClass("hidden");
+});
